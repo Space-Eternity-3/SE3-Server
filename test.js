1a2bef74cd4971c0f71a28f339c79b769fe4e4a9
Nadwey <nadweyyy@gmail.com>
2024-06-12 21:54:34 +0200

    fix test

--- test.js
+++ test.js
@@ -1,13 +1,12 @@
 const WebSocket = require("ws");
 const child_process = require("child_process");
-const { port } = require("./config");
 
 console.log("Spawning server process...");
 const serverProcess = child_process.fork("./index.js");
 
 serverProcess.on("spawn", () => {
     setTimeout(() => {
-        const ws = new WebSocket(`ws://localhost:${port}`);
+        const ws = new WebSocket(`ws://localhost:27683`);
 
         ws.on("open", function open() {
             console.log("Connection successful! ✔️");
